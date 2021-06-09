e4bfbdadd14e7411b214b51cd1b7557748e35ff2
adiporn <kiraso74@gmail.com>
2021-06-09 16:47:40 +0700

    add styling app css

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinDetailPage from "./pages/CoinDetailPage";
+import CoinSummaryPage from "./pages/CoinSummaryPage";
+import Header from "./components/Header";
+import './App.css'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Route exact path="/" component={CoinSummaryPage} />
+    </BrowserRouter>
+  );
+};
+
+export default App;
